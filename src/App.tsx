165b214f842be8106c07b09a14bf10166e876c38
cazser <2264712072@qq.com>
2021-11-06 20:24:10 +0800

    安装react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
-let Button =styled.button 
-`color: red`
+
 function App() {
   return (
     <div className="App">
-     <div>
-      <Button>你好</Button>
-     </div>
+    
     </div>
   );
 }
