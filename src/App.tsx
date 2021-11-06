c79cea4b3092d7f75011323414eb1dc29531a8b2
cazser <2264712072@qq.com>
2021-11-06 20:37:15 +0800

    react-router-dom6更新了api，保险起见使用react-router-dom5

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+Switch,
+  Route,
+  Link }
+from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  
   );
 }
 
