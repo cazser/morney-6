c96afa571ec48745e017c0d9b2a0b66588a8d787
cazser <2264712072@qq.com>
2021-11-08 10:47:29 +0800

    money页面>categorysection样式

--- src/views/Money.jsx
+++ src/views/Money.jsx
@@ -1,45 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 import Layout from '../components/Layout'
+import NotesSection from 'components/NotesSection';
+import TagsSection from 'components/TagsSection';
+import CategorySection from 'components/CategorySection';
 
-const TagsSection = styled.section`
-background: #FFFFFF;padding:12px 16px;
->ol{
-margin: 0 -12px;
->li{
-  background:#D9D9D9;border-radius: 18px;
-  display: inline-block;  padding: 3px 18px;
-  font-size: 14px;  margin: 8px 12px;
-}
-}
-
->button{
-  background: none;  border: none;
-  border-bottom: 1px solid #333;
-  color: #666;  padding: 2px 4px;
-  margin-top:8px;
-}
-`;
-
-
-const NotesSection = styled.section`
-background: #f5f5f5;
-padding: 0px 16px;
-font-size: 14px;
->label{
-  display: flex;
-  align-items: center;
-  >span{margin-right: 16px;white-space: nowrap;}
-  >input{
-    display: block;
-    width: 100%;
-    height: 72px;
-    background: none;
-    border: none;
-  }
-}
-`;
-const CategorySection = styled.section``;
 const NumberPadSection = styled.section``;
 function Money(){
   return(
@@ -61,7 +26,7 @@ function Money(){
     </NotesSection>
     <CategorySection>
     <ul>
-      <li>支出</li>
+      <li className="selected">支出</li>
       <li>收入</li>
     </ul>
     </CategorySection>
