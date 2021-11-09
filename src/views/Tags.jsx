e39a8b1e8b46a12fba809680ddc55b421c2d2415
cazser <2264712072@qq.com>
2021-11-09 15:09:54 +0800

    tags页面完工

--- src/views/Tags.jsx
+++ src/views/Tags.jsx
@@ -1,11 +1,58 @@
 import React, { useState } from 'react'
+import styled from 'styled-components';
 import useTags from 'useTags'
 import Layout from '../components/Layout'
+const TagList = styled.ol`
+font-size: 16px;
+background: white;
+>li{
+  border-bottom: 1px solid #d5d5d9;
+  line-height: 20px;
+  padding: 12px 16px 12px 0;
+  margin-left: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &.right{
+    font-size:20px;
+  }
+}
+`;
+
+const Button =styled.button`
+font-size: 18px;border: none;
+padding: 8px 12px;background: #767676;
+border-radius: 4px;color: white;
+`;
+
+const Center= styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Space=styled.div`
+height: 16px;
+`;
 function Tags(){
   const {tags, setTags} = useTags();
   return(
     <Layout>
-    <div>标签页</div>
+    <TagList>
+      {tags.map((tag)=>{
+        return(
+          <li key={tag}>
+          <span>{tag}</span>
+          <span className="right">&gt;</span></li>
+        )
+      })}
+    </TagList>
+    <Center>
+      <Space/>
+      <Button>新增标签</Button>
+      <Space />
+    </Center>
     </Layout>
   )
 }
