4fad602200f60c16f320097257178843a936f286
cazser <2264712072@qq.com>
2021-11-11 09:04:34 +0800

    按日期分组

--- src/views/Statistics.jsx
+++ src/views/Statistics.jsx
@@ -24,6 +24,24 @@ function Statistics(){
   const [category, setCategory]= useState('-')
   const {records} = useRecords();
   const {getName} = useTags();
+  const hash ={}
+  const displayRecords=
+     records.filter((r)=>{return r.category === category})
+  displayRecords.map((r)=>{
+    const key = day(r.createAt).format('YYYY-MM-DD')
+    
+    if(!(key in hash)){
+      hash[key] = []
+    }
+    hash[key].push(r);
+  })
+
+  const array = Object.entries(hash).sort((a,b)=>{
+    if(a[0]===b[0]) return 0;
+    if(a[0]>b[0]) return -1;
+    if(a[0]<b[0]) return 1;
+  });
+  console.log(array);
   return(
   <Layout>
     
@@ -32,11 +50,16 @@ function Statistics(){
     </WhiteWrapper>
     <div>
       
-      {records.map((r)=>{
+      {array.map(([date, records])=>{
+       return (
+        <div>
+          <h3>{date}</h3>
+          <div>
+          {records.map((r)=>{
         return (
         <Item>
           <div className="tags"> 
-          {r.tagIds.map(tagId=><span>{getName(tagId)}</span>)}
+          {r.tagIds.map(tagId=><span key={tagId}>{getName(tagId)}</span>)}
           </div>
          {r.note && <div className="note">{r.note}</div>}
           
@@ -50,7 +73,11 @@ function Statistics(){
         )
       })}
       
-    </div>
+            </div>
+          </div>
+       )})}
+
+        </div>
     </Layout>
   )
 }
